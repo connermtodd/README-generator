fbffd53c81df0955e48b9c06d849aa5014cff4fe
connermtodd <99088964+connermtodd@users.noreply.github.com>
2022-06-19 15:25:40 -0400

    initial package install and user prompts added

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,36 @@
-// TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'confirm',
+            message: 'Hello. Would you like to create a README.md file for a project? (Required)',
+            validate: confirmInput => {
+                if (confirmInput) {
+                    return true;
+                } else {
+                    console.log('Goodbye.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'README',
+            message: 'Please give a brief description of your project.',
+            when: ({ README }) => {
+                if (README) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        }
+    ]);
+};
+
+    promptUser().then(answers => console.log(answers));
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
